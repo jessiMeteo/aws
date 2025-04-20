d2d21a6594d5ba1efc2e8e38297f374ef2407e20
jessimeteo <jessica.laengle@uibk.ac.at>
2025-04-20 16:57:39 +0200

    Popup mit Text  (Name + Seehöhe) erstellt

--- main.js
+++ main.js
@@ -39,6 +39,23 @@ async function loadStations(url) {
     let jsondata = await response.json();
 
     // Wetterstationen mit Icons und Popups
-
+    console.log(jsondata);
+    //console.log(jsondata.name == 'Hafelekar')
+    L.geoJSON(jsondata, {
+        pointToLayer: function (feature, latlng) {
+            //console.log(feature);
+            return L.marker(latlng, {
+                icon: L.icon({
+                    iconUrl: `icons/wifi.png`,
+                    //iconAnchor: [16, 37], //halbe Bildbreite und ganze Bildhöhe
+                    //popupAnchor: [0, -37] // Damit Popup nicht Icon überdeckt
+                })
+            })
+        }, onEachFeature: function (feature, layer) {
+            layer.bindPopup(`
+                    <h4> ${feature.properties.name} (${feature.geometry.coordinates[2]}m) </h4>
+                `)
+        }
+    }).addTo(overlays.stations);
 }
 loadStations("https://static.avalanche.report/weather_stations/stations.geojson");
